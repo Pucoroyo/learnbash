b5d3ac3e0f7199ab3600b79dda1da5d9512a8261
Pucoroyo <pucoroyo@gmail.com>
2018-08-06 12:52:02 +0700

    Add some project

---
+++ pastgrade.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -n  Instert Your Result:
+
+read angka
+if 	(($angka>70))
+  then 
+	echo "Congrats! You past the test"
+  else 
+	echo "Sorry, you failed the test"
+
+fi
+
+
+
+
